746c57d7db1f932790f2de45b083f0c0b8596e4b
rctandrade <rct.andradeconsultoria@gmail.com>
2025-02-19 16:17:11 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,30 @@
 import Logo from './assets/logo-andrade-costa.png'; // Importe a imagem do logoimport React from 'react';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Services from './components/Services';
+import Team from './components/Team';
+import Social from './components/Social';
+import Contact from './components/Contact';
+import Logo from './assets/logo-andrade-costa.png'; // Importe a imagem do logo
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <img src={Logo} alt="Logo Andrade Costa" className="logo" /> {/* Adicione o logo aqui */}
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Team />
+      <Social />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
